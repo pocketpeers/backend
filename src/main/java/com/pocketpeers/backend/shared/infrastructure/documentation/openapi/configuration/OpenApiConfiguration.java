a5f18c827edaa41c57c97e9afc9a7dd99214c731
Salvador Salinas <145953379+salvadoorssalinas@users.noreply.github.com>
2026-05-19 20:00:16 -0500

    feat(shared): add base entities and config

---
+++ src/main/java/com/pocketpeers/backend/shared/infrastructure/documentation/openapi/configuration/OpenApiConfiguration.java
@@ -0,0 +1,57 @@
+package com.pocketpeers.backend.shared.infrastructure.documentation.openapi.configuration;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.ExternalDocumentation;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+public class OpenApiConfiguration implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods("*")
+                .allowedHeaders("*");
+    }
+
+    @Bean
+    public OpenAPI pocketPartnersOpenApi() {
+
+        var openApi = new OpenAPI();
+        openApi
+                .info(new Info()
+                        .title("B4U Pocket Partners API")
+                        .description("B4U Pocket Partners application REST API documentation.")
+                        .version("v1.0.0")
+                        .license(new License().name("Apache 2.0")
+                                .url("https://springdoc.org")))
+                .externalDocs(new ExternalDocumentation()
+                        .description("B4U Pocket Partners wiki Documentation")
+                        .url("https://github.com/PocketPartners/Informe"));
+
+        final String securitySchemeName = "bearerAuth";
+
+        openApi.addSecurityItem(new SecurityRequirement()
+                        .addList(securitySchemeName))
+                .components(new Components()
+                        .addSecuritySchemes(securitySchemeName,
+                                new SecurityScheme()
+                                        .name(securitySchemeName)
+                                        .type(SecurityScheme.Type.HTTP)
+                                        .scheme("bearer")
+                                        .bearerFormat("JWT")));
+
+        return openApi;
+    }
+}
---
+++ src/main/java/com/pocketpeers/backend/shared/interfaces/rest/resources/MessageResource.java
@@ -0,0 +1,4 @@
+package com.pocketpeers.backend.shared.interfaces.rest.resources;
+
+public record MessageResource(String message) {
+}
